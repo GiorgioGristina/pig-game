b630c2ae24f51e37da2ea2b16e6a95941a12b8d6
Giorgio Gristina <giorgio.gristina91@gmail.com>
2022-02-06 18:16:49 +0000

    refactore the game

--- script.js
+++ script.js
@@ -5,24 +5,35 @@ const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 
 const diceEl = document.querySelector('.dice');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 const btnNew = document.querySelector('.btn--new');
 
+// declare variable
+let currentScore, activePlayer, playing, scores
 
-// starting condition
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden')
- 
-// data
-const scores = [0,0]
-let currentScore = 0;
-let activePlayer = 0
-let playing = true;
-
+// reset function 
+const init = () => {
+  scores = [0,0]
+  currentScore = 0;
+  activePlayer = 0
+  playing = true;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  diceEl.classList.add('hidden')
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+}
+// coll method to reset everything and define variable value
+init()
 
 // switch player function
 const switchPlayer = () => {
@@ -41,19 +52,14 @@ btnRoll.addEventListener("click", () =>{
     // 2. display the relate img dice number
     diceEl.classList.remove('hidden')
     diceEl.src = `dice-${dice}.png`
-    // add the number to the current scoore
-
     // 3. if is 1 pass the turn to the other player
     if (dice !== 1) {
       // add dice to current score
-      currentScore  += dice
-      
-      document.querySelector(`#current--${activePlayer}`).textContent = currentScore // change later to different player
-    
+      currentScore  += dice      
+      document.querySelector(`#current--${activePlayer}`).textContent = currentScore
     }  else {
       // switch player
       switchPlayer()
-
     }
   }
 });
@@ -68,15 +74,15 @@ btnHold.addEventListener("click", () =>{
     // 2. check if players score is >=100
     if (scores[activePlayer] >= 100) {
       playing = false;
+      diceEl.classList.add('hidden')
       // if true end game and say who won
       document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
-      // document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
-  
-      console.log("you won");
     } else {
       //  false switch player
       switchPlayer()
     }    
   }
 });
+
+btnNew.addEventListener("click", init);
 
